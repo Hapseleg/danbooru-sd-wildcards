d81104ebbcae1e5c946097846ea2bc5fa20fad91
NLJ <hapse@hh4.dk>
2024-08-12 21:35:46 +0200

    works nicely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
-import json
 import time
+import sys
+
 
 def get_data(base_url, tag):
     """Fetches JSON data from the specified API URL with pagination.
@@ -31,31 +32,41 @@ def get_data(base_url, tag):
     return processed_data
 
 
-def process_data(tags, toptagscount):
+def process_data(tags, toptagscount, basetag, commontags, filtertags):
     """
 
     """
-    processed_data = ""
-    unique_tags = set(tags)
+    # add the basetag and commontags to the string
+    processed_data = basetag + ', '
+    for commontag in commontags:
+        processed_data += commontag + ', '
+
+    # remove them from the list
+    new_tags_list = tags
+    new_tags_list = [i for i in new_tags_list if i != basetag]
+    for common in commontags:
+        new_tags_list = [i for i in new_tags_list if i != common]
+    for filter in filtertags:
+        new_tags_list = [i for i in new_tags_list if i != filter]
+
+    # create a dict
+    unique_tags = set(new_tags_list)
     tags_count = {}
 
+    # add the tags to a list of tuples with (word, count)
     for tag in unique_tags:
+        tags_count[tag] = new_tags_list.count(tag)
 
-        tags_count[tag] = tags.count(tag)
-        #unique_tags.update({tag:tags.count(tag)})
+    # sort by count
+    sorted_tags = sorted(tags_count.items(), key=lambda x:x[1], reverse=True)
 
+    # add top toptagscount to the string
+    for i in range(toptagscount):
+        processed_data += sorted_tags[i][0] + ', '
 
-
-
-    print(tags_count)
-
+    processed_data += "\n"
 
     return processed_data
-
-
-
-
-
 
 
 
@@ -70,6 +81,7 @@ def main():
     parser = argparse.ArgumentParser(description="Extract certain tags from Danbooru, for Stable diffusion wildcard files")
     parser.add_argument("--tags", help="The tags, for example, 'cat,bird,dog' - csv", required=True)
     parser.add_argument("--commontags", help="If you want specific common tags, such as 'no_humans,simple_background' or '1girl' etc (optional) - csv", required=False)
+    parser.add_argument("--filtertags", help="If you want filter tags, such as 'solo' (optional) - csv", required=False)
     parser.add_argument("--toptagscount", help="Specify the amount of tags you want added, defaults to 5 (optional) - number", required=False)
     parser.add_argument("--filename", help="If you want it to save it as a txt file in current dir (optional) - string", required=False)
     parser.add_argument("--username", help="The username to use with the API(optional) - string", required=False)
@@ -86,10 +98,14 @@ def main():
     base_url = "https://danbooru.donmai.us/posts.json?"  # Base URL, limit 200 and tag order:favcount
     tags = args.tags.split(",")
     commontags = []
+    filtertags = []
     toptagscount = 5
 
     if args.commontags:
         commontags = args.commontags.split(",") 
+
+    if args.filtertags:
+        filtertags = args.filtertags.split(",")
 
     if args.limit:
         base_url += f"limit={args.limit}"
@@ -101,7 +117,9 @@ def main():
     if args.apikey:
         base_url += f"&login={args.username}"
         base_url += f"&api_key={args.apikey}"
-
+    else:
+        if (len(commontags) + len(filtertags) + 1) > 2:
+            sys.exit("Free users can max search for 2 tags at a time.")
     
 
     #add tags
@@ -109,6 +127,9 @@ def main():
     # add common tags
     for commontag in commontags:
         base_url += "+" + commontag
+
+    for filter in filtertags:
+        base_url += "+" + filter
         #base_url = base_url[:-1]
 
 
@@ -119,38 +140,21 @@ def main():
 
 
 
-    all_data = []
+    wildcard_string_list = []
 
     for tag in tags:
         data = get_data(base_url, tag)
-        process_data(data, toptagscount)
+        wildcard_string = process_data(data, toptagscount,tag, commontags, filtertags)
+        wildcard_string_list.append(wildcard_string)
+        time.sleep(1)
+
+    if args.filename:
+        f = open(f"{args.filename}.txt", "a")
+        for s in wildcard_string_list:
+            f.write(s)
+        f.close()
         
 
-
-    # for page in range(1, args.pages + 1):
-    #     page_data = get_data(api_url, page, args.limit)
-
-    #     # Break if nothing is returned because that means... theres nothing left?
-    #     if page_data.count == 0:
-    #         break
-
-    #     all_data.extend(page_data)
-
-    #     #you can do more calls than this if youre gold or platinum but eh... No rush for me, check here for info: https://danbooru.donmai.us/wiki_pages/help%3Ausers
-    #     time.sleep(1)
-
-    # # for item in all_data:
-    # #     tags = item["tags"]
-    # #     for tag in tags:
-    # #         total_fav_tags[tag] = total_fav_tags.get(tag, 0) + 1
-    # #         if just_the_tags.__contains__(tag) == False:
-    # #             just_the_tags.append(tag)
-
-    # # Sort by the tag with the highest value!
-    # sorted_tags = sorted(total_fav_tags.items(), key=itemgetter(1), reverse=True)
-
-    # save_to_json(sorted_tags, "./files/", "total_fav_tags", ".json", False)
-    # print("Total fav tags saved to total_fav_tags.json")
 
 if __name__ == "__main__":
     main()
